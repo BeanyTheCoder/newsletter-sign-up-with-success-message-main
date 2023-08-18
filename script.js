f746d563125e41f8694fed373cf9f535a9f9e4ad
Bean <alexanderafoko@gmail.com>
2023-08-18 12:47:13 +0000

    changes

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
-let emailInput = document.querySelector("#email");
-let submitButton = document.querySelector("#submit");
-let regex = /^([\.\_a-zA-Z0-9]+)@([a-zA-Z]+)\.([a-zA-Z]){2,8}$/;
+let emailInput = document.querySelector("#emailInput");
+let emailText = document.querySelector('#emailText')
+let form = document.querySelector(".container__main__form");
+let emailRegex = /^([\.\_a-zA-Z0-9]+)@([a-zA-Z]+)\.([a-zA-Z]){2,8}$/;
 
 function validateEmail() {
-  console.log("e");
-  let valid = regex.test(emailInput.value);
-
+  let valid = emailRegex.test(emailInput.value);
   return valid;
 }
 
 emailInput.addEventListener("input", () => {
   if (validateEmail()) {
+    emailInput.classList.remove("invalid");
     emailInput.classList.add("valid");
   } else {
+    emailInput.classList.remove("valid");
     emailInput.classList.add("invalid");
   }
 });
+
+form.addEventListener('submit', (event) => {
+  emailText.textContent = emailInput.value    
+  console.log('e')
+})
+
